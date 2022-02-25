94b7167e12f113973a7a08550fdc19c862109a27
Saul Wegrzyn <saul@wegrzyn.org>
2022-02-25 14:57:59 +0000

    statted on editgs:

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
 import streamlit as st
-
+import requests
+import pandas as pd
+from datetime import datetime
+import pytz
 '''
 # TaxiFareModel front
 '''
-
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
+### Input your Passenger Numbers:
+st.select_slider('Slide to select', options=[1,2,3,4,5,6,7,8])
 
 
 '''
@@ -34,22 +20,52 @@ See ? No need to load a `model.joblib` file in this app, we do not even need to 
 
 url = 'https://taxifare.lewagon.ai/predict'
 
-if url == 'https://taxifare.lewagon.ai/predict':
+'''
+2. Let's build a dictionary containing the parameters for our API...
+'''
+pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
+eastern = pytz.timezone("US/Eastern")
+localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+params_dict = {
+"key": '2013-07-06 17:18:00.000000119',
+"pickup_datetime": formatted_pickup_datetime,
+"pickup_longitude": pickup_longitude,
+"pickup_latitude": pickup_latitude,
+"dropoff_longitude": dropoff_longitude,
+"dropoff_latitude": dropoff_latitude,
+"passenger_count": passenger_count
+}
 
 '''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-
 3. Let's call our API using the `requests` package...
+'''
+response = requests.get(url,params=params_dict).json()
 
+'''
 4. Let's retrieve the prediction from the **JSON** returned by the API...
 
 ## Finally, we can display the prediction to the user
 '''
 
-params_dict = {
+print(response)
 
-}
+if __name__ == '__main__':
+    pickup_datetime='2012-10-06 12:10:20'
+    pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
+    eastern = pytz.timezone("US/Eastern")
+    localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+    utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+    formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
+    params_dict = dict(
+    pickup_datetime='2012-10-06 12:10:20',
+    pickup_longitude=40.7614327,
+    pickup_latitude=-73.9798156,
+    dropoff_longitude=40.6413111,
+    dropoff_latitude=-73.9797156,
+    passenger_count=1
+    )
+    response = requests.get(url,params=params_dict).json()
+    print(response)
